da73515294367dbb7562a1692859fa4ee4407fe9
Juan Carlos Vega Abarca <juanvegab23@gmail.com>
2015-03-31 00:22:10 -0600

    Adding facebook authentication angularjs code

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,15 +8,29 @@
  * Controller of the firebaseApp
  */
 angular.module('firebaseApp')
-.controller('MainCtrl', ['$scope', 'Quotes', 'Tags', function ($scope, Quotes, Tags) {
+.controller('MainCtrl', ['$scope', 'Quotes', 'Tags', '$firebaseAuth', 
+				function ($scope, Quotes, Tags, $firebaseAuth) {
 	
 	this.quotes = Quotes;
 	this.tags = Tags;
-
 	this.quote = {author: '', quote: '', date: new Date(), tags: []};
-	
+	this.auth = null;
+	this.authData = null;
+
+	var self = this;
+
 	this.addQuote = function() {
 
+	};
+
+	this.authenticate = function() {
+		this.auth = $firebaseAuth(new Firebase('https://torrid-fire-8997.firebaseio.com/'));
+		this.auth.$authWithOAuthPopup("facebook").then(function(authData) {
+			self.authData = authData;
+			console.log("Logged in as:", authData.uid);
+		}).catch(function(error) {
+			console.log("Authentication failed:", error);
+		});
 	};
 	
 	$('.carousel').carousel();
